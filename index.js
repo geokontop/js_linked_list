b430c9631a042e3c09eabec546ce4fd92446055d
geokontop <geokontop1960@gmail.com>
2018-12-23 21:34:27 +0200

    2. works

--- index.js
+++ index.js
@@ -2,10 +2,17 @@
 const LinkedList = require('./src/linkedList')
 
 var ll = new LinkedList();
-
+ 
+ll.addToHead(123);
+ll.addToHead(70);
+ll.addToHead('hello');
+ll.addToTail(19);
+ll.addToTail('world');
+ll.addToTail(20);
 ll.addToTail(567)
 ll.addToTail(333)
 ll.addToTail(111)
 ll.addToHead(4)
-console.log(ll);
-console.log(ll.tail.prev.prev.prev);
+// console.log(ll);
+console.log(ll.head.next.value);
+console.log(ll.tail.prev.prev.prev.prev.value);
--- src/linkedList.js
+++ src/linkedList.js
@@ -27,8 +27,42 @@ LinkedList.prototype.addToTail = function (value) {
 }
 
 LinkedList.prototype.removeHead = function () {
-
+    if (!this.head) return null;
+    var val = this.head.value;
+    this.head = this.head.next;
+    if (this.head) this.head.prev = null;
+    else this.tail = null;
+    return val;
 }
 
+LinkedList.prototype.removeTail = function() {
+    if (!this.tail) return null;
+    var val = this.tail.value;
+    this.tail = this.tail.prev;
+    if (this.tail) this.tail.next = null;
+    else this.head = null;
+    return val;
+};
+
+LinkedList.prototype.search = function(searchValue) {
+    var currentNode = this.head;
+    while (currentNode) {
+        if (currentNode.value === searchValue) return currentNode.value;
+        currentNode = currentNode.next;
+    } 
+    return null;
+};
+   
+LinkedList.prototype.indexOf = function(value) {
+    var indexes = [];
+    var currentIndex = 0;
+    var currentNode = this.head;
+    while(currentNode) {
+        if (currentNode.value === value) indexes.push(currentIndex);
+        currentNode = currentNode.next;
+        currentIndex++;
+    }
+    return indexes;
+};
 
 module.exports = LinkedList;
